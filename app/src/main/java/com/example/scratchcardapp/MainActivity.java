a9117d1b35e4dfdb053c71daf681a2f596a1d644
PavlaManova <48806251+PavlaManova@users.noreply.github.com>
2021-08-15 18:06:36 +0300

    Create android app
    with imageView for owner image

---
+++ app/src/main/java/com/example/scratchcardapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.scratchcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
