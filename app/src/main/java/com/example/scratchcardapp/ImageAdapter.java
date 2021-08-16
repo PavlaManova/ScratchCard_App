ac3de8df6a365d22a45fc2eeca91ff6315696c66
PavlaManova <48806251+PavlaManova@users.noreply.github.com>
2021-08-16 17:08:12 +0300

    movies' pictures showing in rounded-corners shape
    - grid starts after owner image (bug fixed by brute force)

--- app/src/main/java/com/example/scratchcardapp/ImageAdapter.java
+++ app/src/main/java/com/example/scratchcardapp/ImageAdapter.java
@@ -1,23 +1,35 @@
 package com.example.scratchcardapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.imageview.ShapeableImageView;
 
 public class ImageAdapter extends BaseAdapter {
-    private Context context;
 
-    public Integer[] images={
+    private Context context;
+    private LayoutInflater inflater;
+
+    private Integer[] images={
             R.drawable.lion,
             R.drawable.the_godfather,
             R.drawable.the_kite_runner,
             R.drawable.titanic,
             R.drawable.troy
+    };
+
+    private String[] moviesNames= {
+            "Lion",
+            "The Godfather",
+            "The Kite Runner",
+            "Titanic",
+            "Troy"
     };
 
     public ImageAdapter(Context c){
@@ -41,15 +53,30 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        ShapeableImageView imageView2=new ShapeableImageView(context);
+        if(inflater==null)
+        {
+            inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        if(view==null)
+        {
+            view=inflater.inflate(R.layout.row_item,null);
+        }
+
+        ImageView imageView=view.findViewById(R.id.image_view);
+        TextView textView=view.findViewById(R.id.text_view);
+        imageView.setImageResource(images[i]);
+        textView.setText(moviesNames[i]);
+
+        return view;
+        /*ShapeableImageView imageView2=new ShapeableImageView(context);
         imageView2.setImageResource(images[i]);
         imageView2.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        imageView2.setLayoutParams(new GridView.LayoutParams(250,250));
+        imageView2.setLayoutParams(new GridView.LayoutParams(250,250));*/
         
-        ImageView imageView=new ImageView(context);
+        /*ImageView imageView=new ImageView(context);
         imageView.setImageResource(images[i]);
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         imageView.setLayoutParams(new GridView.LayoutParams(250,250));
-        return imageView;
+        return imageView;*/
     }
 }
