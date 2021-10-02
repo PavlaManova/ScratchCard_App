4471a86832ef5bcc30489fa5c007ef0c356a91b8
PavlaManova <48806251+PavlaManova@users.noreply.github.com>
2021-10-02 21:40:13 +0300

    scratch feature
    when scratched movie is clicked only the image is shown

--- app/src/main/java/com/example/scratchcardapp/ClickedItemActivity.java
+++ app/src/main/java/com/example/scratchcardapp/ClickedItemActivity.java
@@ -1,25 +1,33 @@
 package com.example.scratchcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.anupkumarpanwar.scratchview.ScratchView;
+import com.google.android.material.imageview.ShapeableImageView;
 
 public class ClickedItemActivity extends AppCompatActivity {
 
     private ImageView movieToScrImg;
+    private ImageView scratchedImg;
     private TextView movieName;
+    private ScratchView scratchView;
     private String selectedName;
     private int selectedImage;
     private int position;
     private Intent intent;
     private Boolean isScratched;
-
-    Button btn;
+    private CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,37 +36,48 @@ public class ClickedItemActivity extends AppCompatActivity {
 
         movieToScrImg=(ImageView)findViewById(R.id.imgToScratch);
         movieName=(TextView)findViewById(R.id.movieNameView);
+        scratchView=findViewById(R.id.scratchView);
+        cardView=findViewById(R.id.cardView);
+        scratchedImg=(ImageView)findViewById(R.id.scratchedImg);
 
-        btn=(Button)findViewById(R.id.tryBtn);
 
         intent=getIntent();
         getItemExtras();
 
-        movieToScrImg.setImageResource(selectedImage);
-        //loadDataAccordingScratched();
 
-        //temporary code
-        btn.setOnClickListener(new View.OnClickListener() {
+        loadDataAccordingScratched();
+
+
+        scratchView.setStrokeWidth(15);
+
+        scratchView.setRevealListener(new ScratchView.IRevealListener() {
             @Override
-            public void onClick(View v) {
+            public void onRevealed(ScratchView scratchView) {
+                Toast.makeText(getApplicationContext(),"Hope you liked it!",Toast.LENGTH_SHORT).show();
                 isScratched=true;
-                btn.setEnabled(false);
                 PrefConfig.saveScratchedImgInPref(getApplicationContext(),isScratched,position);
             }
+
+            @Override
+            public void onRevealPercentChangedListener(ScratchView scratchView, float percent) {
+
+            }
         });
+
     }
 
     private void loadDataAccordingScratched() {
+        movieToScrImg.setImageResource(selectedImage);
         isScratched=PrefConfig.loadScratchedImgFromPref(getApplicationContext(),position);
+        scratchedImg.setImageResource(selectedImage);
 
         if(isScratched){
-            movieToScrImg.setImageResource(selectedImage);
-            btn.setEnabled(false);
+            cardView.setVisibility(View.INVISIBLE);
+            scratchedImg.setVisibility(View.VISIBLE);
         }
-        else {
-            //implement code for scratching
-            movieToScrImg.setImageResource(R.drawable.default_min);
-            btn.setEnabled(true);
+        else{
+            cardView.setVisibility(View.VISIBLE);
+            scratchedImg.setVisibility(View.INVISIBLE);
         }
     }
 
